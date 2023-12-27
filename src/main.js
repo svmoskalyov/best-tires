69ec4c3fbed431102d170e4462d886e0967bb566
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-27 11:49:01 +0200

    added icons

--- src/main.js
+++ src/main.js
@@ -8,7 +8,49 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
+import {
+  faEye,
+  faEyeSlash,
+  faImage,
+  faPlus,
+  faMinus,
+  faFilter,
+  faRotate,
+  faSpinner,
+  faArrowRightToBracket,
+  faArrowRightFromBracket,
+  faXmark,
+  faCartShopping,
+  faArrowDownWideShort,
+  faArrowDownShortWide,
+  faHeart as fasHeart
+} from '@fortawesome/free-solid-svg-icons'
 
+library.add([
+  faEye,
+  faEyeSlash,
+  faImage,
+  faPlus,
+  faMinus,
+  faFilter,
+  faRotate,
+  faSpinner,
+  faArrowRightToBracket,
+  faArrowRightFromBracket,
+  faXmark,
+  faCartShopping,
+  fasHeart,
+  faArrowDownWideShort,
+  faArrowDownShortWide,
+  farHeart,
+  faLinkedin
+])
+
+app.use(router)
+app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
