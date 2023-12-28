04f55ee5e4d44a4662ea43e4b4b6af48946ce9e1
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-28 19:01:29 +0200

    added Footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,23 @@
 <script setup>
 import Container from '@/components/shared/AppContainer.vue'
+import AppButton from './shared/AppButton.vue'
 </script>
 
 <template>
   <footer class="footer">
     <Container>
       <div class="footer__content">
-        <div class="footer__copyright">&copy; 2024 [name]</div>
-        <div class="footer__link">in</div>
+        <div class="footer__copyright">&copy; 2024 Serhii Moskalov</div>
+        <div class="footer__link">
+          <a
+            href="https://www.linkedin.com/in/serhii-moskalov"
+            target="_blank"
+          >
+            <AppButton icon>
+              <font-awesome-icon :icon="['fab', 'linkedin']" />
+            </AppButton>
+          </a>
+        </div>
       </div>
     </Container>
   </footer>
@@ -23,5 +33,5 @@ import Container from '@/components/shared/AppContainer.vue'
     display: flex
     justify-content: space-between
     align-items: center
-    height: 40px
+    height: 48px
 </style>
