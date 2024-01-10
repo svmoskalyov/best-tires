4669a559057b96e2972bdfc4ed6ddfb29e5492e9
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-10 16:38:21 +0200

    Create SignUpView.vue

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import FormAuth from '@/components/FormAuth.vue'
+</script>
+
+<template>
+  <div class="signup-page">
+    <FormAuth
+      title="Create an account"
+      lable-btn="Sign up"
+    />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.signup-page
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100%
+</style>
