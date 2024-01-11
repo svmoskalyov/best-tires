f5b6220461e58f76d1bc14708c04da40c6db79c4
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-11 11:10:22 +0200

    added Sign in

---
+++ src/views/SignInView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import FormAuth from '@/components/FormAuth.vue'
+</script>
+
+<template>
+  <div class="signup-page">
+    <FormAuth
+      title="Welcome back"
+      lable-btn="Sign in"
+    />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.signup-page
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100%
+</style>
