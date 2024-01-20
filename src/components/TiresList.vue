2ce63f2fb4e8a280fc7e9a66fe7a7fe42901e2b7
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-20 20:00:35 +0200

    Create TiresList.vue

---
+++ src/components/TiresList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TiresListItem from './TiresListItem.vue'
+
+const props = defineProps({
+  tires: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
+</script>
+
+<template>
+  <div class="tires-list">
+    <TiresListItem
+      v-for="tire of tires"
+      :key="tire.id"
+      :tire="tire"
+    />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+// .tires-list
+</style>
