398ee9b75d2673cceffd41b269b023ef7731ee8c
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-21 14:07:49 +0200

    Update TiresListItem.vue

--- src/components/TiresListItem.vue
+++ src/components/TiresListItem.vue
@@ -13,6 +13,15 @@ const props = defineProps({
 const favorites = ref(false)
 // const hover = ref(false)
 const hover = ref(true)
+const infoArr = ref([
+  {
+    name: 'Brand',
+    text: props.tire.brand[0].toUpperCase() + props.tire.brand.slice(1)
+  },
+  { name: 'Type', text: props.tire.type },
+  { name: 'Season', text: props.tire.season },
+  { name: 'Diameter', text: props.tire.diameter }
+])
 
 function tireName() {
   const { brand, width, profile, diameter } = props.tire
@@ -34,13 +43,22 @@ function toogleFavorites() {
   > -->
 
   <div class="card">
-    <img
-      class="card-img"
-      :src="tire.picture"
-      alt="image tire"
-      width="100"
-      height="100"
-    />
+    <div class="img-wrapper">
+      <img
+        v-if="tire.picture"
+        class="card-img"
+        loading="lazy"
+        width="232"
+        height="232"
+        alt="image tire"
+        :src="tire.picture"
+      />
+      <font-awesome-icon
+        v-else
+        class="card-img card-img-def"
+        :icon="['fas', 'image']"
+      />
+    </div>
 
     <div class="card-info">
       <h2 class="card-name">{{ tireName() }}</h2>
@@ -67,7 +85,7 @@ function toogleFavorites() {
       v-if="hover"
       class="info"
     >
-      <ul class="info-list">
+      <!-- <ul class="info-list">
         <li class="info-item">
           <span class="info-name">Brand:</span>
           <span class="info-dots"></span>
@@ -90,6 +108,18 @@ function toogleFavorites() {
           <span class="info-dots"></span>
           <span class="info-text">{{ tire.diameter }}</span>
         </li>
+      </ul> -->
+
+      <ul class="info-list">
+        <li
+          v-for="info of infoArr"
+          :key="info.name"
+          class="info-item"
+        >
+          <span class="info-name">{{ info.name }}</span>
+          <span class="info-dots"></span>
+          <span class="info-text">{{ info.text }}</span>
+        </li>
       </ul>
 
       <AppButton
@@ -104,23 +134,35 @@ function toogleFavorites() {
 @import '../assets/styles/main'
 
 .card
-  // position: relative
+  position: relative
   width: 274px
   background: $background
   border: 1px solid $border
   border-radius: 7px
-  transition: all 0.3s ease
   padding: 20px
 
 .card:hover
   box-shadow: 0px 2px 22px 2px rgba(108,25,179,0.5)
+
+.img-wrapper
+  display: flex
+  justify-content: center
+  align-items: center
+  margin-bottom: 20px
+  width: 232px
+  height: 232px
+
 .card-img
   width: 100%
+  height: 100%
   border-top-left-radius: 7px
   border-top-right-radius: 7px
   background: $background
   transition: all 0.3s ease
-  margin-bottom: 20px
+
+.card-img-def
+  width: 30%
+  height: 30%
 
 .card:hover .card-img
   transform: scale(1.1)
@@ -141,11 +183,20 @@ function toogleFavorites() {
 .card-btn, .info-btn
   width: 100%
 
-// .info
-//   position: absolute
-//   bottom: -170px
-//   left: 0
-//   z-index: 3
+.info
+  position: absolute
+  bottom: -180px
+  left: -1px
+  padding: 20px
+  width: 274px
+  background: $background
+  border: 1px solid $border
+  border-top: none
+  border-bottom-left-radius: 7px
+  border-bottom-right-radius: 7px
+  box-shadow: 0px 12px 22px 2px rgba(108,25,179,0.5)
+  z-index: 3
+
 .info-list
   display: flex
   flex-direction: column
