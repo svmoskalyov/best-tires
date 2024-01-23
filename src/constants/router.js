0dbce8b098cbebda5a3d21ca0b17eb072f5fd09f
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-23 09:29:37 +0200

    Update router.js

--- src/constants/router.js
+++ src/constants/router.js
@@ -1,6 +1,9 @@
 export const ROUTES_PATHS = {
   HOME: '/',
   TIRES: '/tires',
+  TIRES_ID: '/tires/:id',
+  FAVORITES: '/favorites',
+  CART: '/cart',
   ABOUT: '/about',
   SIGNUP: '/signup',
   SIGNIN: '/signin',
