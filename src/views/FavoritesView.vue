1edfefdf95fd5d37fb85f23c67cfca177304fbfa
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-23 09:30:05 +0200

    Create FavoritesView.vue

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TiresList from '@/components/TiresList.vue'
+import { useTiresStore } from '@/stores/tires'
+
+const tiresStore = useTiresStore()
+</script>
+
+<template>
+  <div class="favorites">
+    <h2>favorites view</h2>
+    <span v-if="showLoader">loading...</span>
+
+    <TiresList :tires="tiresStore.favorites" />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.favorites
+  padding: 20px
+  min-height: 100%
+</style>
