9a84150ffd5e6a7c9a5fcd951e5a6bf6892682a8
Serhii Moskalov <sv.moskalyov@gmail.com>
2024-01-28 19:58:11 +0200

    Update TiresView.vue

--- src/views/TiresView.vue
+++ src/views/TiresView.vue
@@ -1,38 +1,14 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import axiosApiInstance from '@/services/axios'
 import TiresList from '@/components/TiresList.vue'
 import { useTiresStore } from '@/stores/tires'
-import firebase from '@/services/firebase'
 
-const showLoader = ref(false)
 const tiresStore = useTiresStore()
 
-const getAllTires = async () => {
-  showLoader.value = true
-  try {
-    const response = await axiosApiInstance.get(
-      `${firebase.databaseURL}/tires.json`
-    )
-    console.log(response)
-    console.log(response.data)
-    // tires.value = response.data
-  } catch (err) {
-    console.log(err.response)
-  } finally {
-    showLoader.value = false
-  }
-}
-
-onMounted(async () => {
-  // await getAllTires()
-})
+if (tiresStore.tires.length === 0) tiresStore.getAllTires()
 </script>
 
 <template>
   <div class="tires">
-    <span v-if="showLoader">loading...</span>
-
     <TiresList :tires="tiresStore.tires" />
   </div>
 </template>
